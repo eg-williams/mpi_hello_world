aaecd95a5d4c764da462adb0094279c0768286ea
Emyr Williams <egwilliams2002@googlemail.com>
2022-05-11 16:35:25 +0100

    Hello world in mpi

---
+++ hello/mpi_hello_world.c
@@ -0,0 +1,24 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    // Initialize MPI env
+    MPI_Init(NULL, NULL);
+
+    // get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // get the rank of the process
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    // get the name of the processor
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
+
+    printf("Hello world, from processor %s, rank %d and out of %d processor\n", processor_name, world_rank, world_size);
+
+    MPI_Finalize();
+}
